4c353074c3548f6e1550fe94c3c4e15cd0d17ea9
girishkalmegh <83416334+girishkalmegh@users.noreply.github.com>
2021-05-07 21:41:46 +0530

    Add files via upload

---
+++ Assignment1/input.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class input
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter first no:");
+int i=sc.nextInt();
+System.out.println("enter second no:");
+int j=sc.nextInt();
+
+
+int k=i*j;
+System.out.println("multiplication is:"+k);
+}
+}

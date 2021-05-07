104cfe7c039079233d28b58874643d3c4f485bcd
girishkalmegh <83416334+girishkalmegh@users.noreply.github.com>
2021-05-07 21:43:20 +0530

    Add files via upload

---
+++ Assignment1/Hello1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Hello1
+{
+public static void main(String args[])
+{
+System.out.println("Hi");
+
+String s1=args[0];
+System.out.println("Hello"+s1);
+
+Scanner sc=new Scanner(System.in);
+String s2=sc.nextLine();
+System.out.println("Hello2"+s2);
+}
+}

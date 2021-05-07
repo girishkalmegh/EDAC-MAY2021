743b0d2064d73d632796800c44470fc45a27cbcf
girishkalmegh <83416334+girishkalmegh@users.noreply.github.com>
2021-05-07 21:31:04 +0530

    Add files via upload

---
+++ Assignment1/DataType.java
@@ -0,0 +1,20 @@
+public class DataType
+{
+public static void main(String args[])
+{
+boolean b=false;
+System.out.println(b);
+
+char b1='A';
+System.out.println(b1);
+
+byte b2=105;
+System.out.println(b2);
+
+short b3=331;
+System.out.println(b3);
+
+long b4=67676767L;
+System.out.println(b4);
+}
+}
---
+++ Assignment1/cmd1.java
@@ -0,0 +1,9 @@
+public class cmd1
+{
+public static void main(String args[])
+{
+System.out.println("MY name is "+args[0]);
+System.out.println("and my favourite number is "+args[1]);
+System.out.println("my nickname is "+args[2]);
+}
+}
